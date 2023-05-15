56402ae9ea974bb00b9e55a21bb667efc3811b8d
unpeko <132242919+unpeko@users.noreply.github.com>
2023-05-15 14:55:21 +1200

    Add files via upload

---
+++ 04_how_much_v2.py
@@ -0,0 +1,27 @@
+# Functions go here...
+def num_check(question, low, high):
+  error = "Please enter an whole number between 1 and 10\n"
+  
+  valid = False
+  while not valid:
+      try:
+        #ask the question
+        response = int(input(question))
+        # if the amount is too low / too high give 
+        if low < response <= high:
+          return response 
+    
+        # output an error 
+        else:
+            print(error)
+  
+      except ValueError:
+        print(error)    
+
+
+# Main routine goes here...
+how_much = num_check("How much would you "
+                    "like to play with? ", 0, 10)
+
+print("You will be spending ${}".format(how_much))
+
